c67a274e6cbd0431ce2e225d66ad3941bea5c9f5
embaby <engamaeae@gmail.com>
2020-05-24 09:28:57 +0200

    Add GetAll Roles

---
+++ E_Commerce/Controllers/RolesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using E_Commerce.Models.FormsData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Controllers
+{
+    public class RolesController:Controller
+    {
+        private readonly RoleManager<IdentityRole> _rolesManager;
+
+        public RolesController(RoleManager<IdentityRole> rolesController)
+        {
+            _rolesManager = rolesController;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Add(CreateRolesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole
+                {
+                    Name = model.Role
+                };
+               var state= await _rolesManager.CreateAsync(role);
+               return state!=null? (IActionResult) RedirectToAction("Products", "Product"):
+                   View("Add");
+            }
+
+            return View("Add");
+        }
+
+        public IActionResult GetAll()
+        {
+            var l = _rolesManager.Roles.AsNoTracking().ToList();
+            return View(l);
+        }
+        
+    }
+}
---
+++ E_Commerce/ViewModel/CreateRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models.FormsData
+{
+    public class CreateRolesViewModel
+    {
+        [Required]
+        public string Role { set; get; }
+    }
+}
